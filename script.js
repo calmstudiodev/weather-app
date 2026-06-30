2c33432f03f03974044c3fba8df9362cc5e64d09
Calm studio <251784941+calmstudiodev@users.noreply.github.com>
2026-06-30 12:04:09 +0300

    Update weather app UI and features

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const apiKey = "ef6893cff61ed83287b1fc895bd3c031";
 const locationInput = document.getElementById("locationInput");
 const searchButton = document.getElementById("searchButton");
 
+
 const locationElement = document.getElementById("location");
+const countryElement = document.getElementById("country");
 const temperatureElement = document.getElementById("temperature");
 const descriptionElement = document.getElementById("description");
 const dateTimeElement = document.getElementById("dateTime");
 const humidityElement = document.getElementById("humidity");
 const windElement = document.getElementById("wind");
+const visibilityElement = document.getElementById("visibility");
+const pressureElement = document.getElementById("pressure");
+const sunriseElement = document.getElementById("sunrise");
+const sunsetElement = document.getElementById("sunset");
+const tempMaxElement = document.getElementById("tempMax");
+const tempMinElement = document.getElementById("tempMin");
 const weatherIcon = document.getElementById("weatherIcon");
 const feelsLikeElement = document.getElementById("feelsLike");
 const weatherInfo = document.querySelector(".weather-info");
@@ -56,12 +64,20 @@ function fetchWeather(location) {
                     minute: "2-digit"
                 });
 
+            const regionNames = new Intl.DisplayNames(["en"], {
+                type: "region",
+            });
+
             locationElement.textContent = data.name;
+
+
+
+            countryElement.textContent = regionNames.of(data.sys.country);
 
             temperatureElement.textContent =
                 `${Math.round(data.main.temp)}°C`;
 
-                feelsLikeElement.textContent = `Feels Like ${Math.round(data.main.feels_like)}°C`;
+            feelsLikeElement.textContent = `Feels Like ${Math.round(data.main.feels_like)}°C`;
 
             descriptionElement.textContent =
                 data.weather[0].description;
@@ -72,13 +88,36 @@ function fetchWeather(location) {
             windElement.textContent =
                 Math.round(data.wind.speed * 3.6) + " km/h";
 
+            pressureElement.textContent = data.main.pressure + "hPa";
+            visibilityElement.textContent = Math.round(data.visibility / 1000) + " km";
+
+            tempMaxElement.textContent = Math.round(data.main.temp_max) + "°C";
+            tempMinElement.textContent = Math.round(data.main.temp_min) + "°C";
+
+            const sunrise = new Date((data.sys.sunrise + data.timezone) * 1000);
+
+            sunriseElement.textContent = sunrise.toLocaleTimeString("tr-TR", {
+                timeZone: "UTC",
+                hour: "2-digit",
+                minute: "2-digit"
+            });
+
+            const sunset = new Date((data.sys.sunset + data.timezone) * 1000);
+
+            sunsetElement.textContent = sunset.toLocaleTimeString("tr-TR", {
+                timeZone: "UTC",
+                hour: "2-digit",
+                minute: "2-digit"
+            });
+
             weatherInfo.style.display = "block";
             weatherIcon.style.display = "block";
 
-            
 
-            weatherIcon.src =  `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-               
+
+            weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            weatherIcon.alt = data.weather[0].description;
+
 
             const weather = data.weather[0].main;
 
